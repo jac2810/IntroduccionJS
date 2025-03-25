3f5855aaf32a5a045b82f94f8abe93ad7aad2a9c
jac2810 <jesuscarrillof@gmail.com>
2025-03-24 22:49:39 -0400

    git commit

---
+++ js/01.js
@@ -0,0 +1,22 @@
+//Variables
+
+var producto = 'Audifnos Gamer'; //Iniciar Variable y asignarle valor
+
+var disponible; //Solo se incia la variable
+
+// producto = true; //Reasignando el valor de la variable
+
+//Inicializar varias variables a la vez
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//Estilos para las variables 
+var nombre_producto = 'Monitor HD' //underscore
+var nombreProducto = 'Monitor HD' //Camelcase
+var NombreProducto = 'Monitor HD' //Pascal Case (Se utiliza para las clases)
+var nombreproducto = 'Monitor HD'
+
+let a = 1 * 2 + ((5 / 2) % 2) 
+
+console.log(a)

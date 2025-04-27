14924e993dff73250d27eab008278732c25abdd2
jac2810 <jesuscarrillof@gmail.com>
2025-04-26 22:12:53 -0400

    git commit

--- js/33.js
+++ js/33.js
@@ -1,9 +1,27 @@
-function obtenerEmpleados(){
-    
-    
+async function obtenerEmpleados(){
+        
    const archivo = 'empleados.json';
 
-   fetch(archivo)
+// //Utilizar fetch para traer datos de una "base de datos" con promesas
+
+//    fetch(archivo)
+//    .then( resultado => resultado.json())
+//    .then( datos => {
+//       // console.log(datos.empleados)
+
+//       const { empleados }= datos //se hace destructuring para extraer el arreglo de empleados del archivo json hacia una variable que podamos luego mostrar por consola:
+//       console.log(empleados)
+//    })
+
+//Utilizar fetch para traer datos de una "base de datos" con async/await
+
+   const resultado = await fetch(archivo);
+   const datos = await resultado.json();
+   console.log(datos)
+
+
 }
+
+
 
 obtenerEmpleados();
